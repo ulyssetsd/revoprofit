0fedad3feb57edf61d047ac2188a86a78e52acca
Ulysse Tassidis <31815920+ulyssetsd@users.noreply.github.com>
2023-02-25 01:51:55 +0100

    improve stock test coverage and accuracy (#22)
    * improve stock test coverage and accuracy
    
    * move models into the correct folder

---
+++ RevoProfit.Core/Stock/Models/StockSellAnnualReport.cs
@@ -0,0 +1,8 @@
+﻿namespace RevoProfit.Core.Stock.Models;
+
+public record StockSellAnnualReport
+{
+    public required IEnumerable<StockSellOrder> StockSellOrders { get; init; }
+    public required decimal Gains { get; init; }
+    public required decimal GainsInEuro { get; init; }
+}

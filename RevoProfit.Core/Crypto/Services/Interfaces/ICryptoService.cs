331d2f399bad5134ad507e698427ceb7af6ceb9c
Ulysse Tassidis <31815920+ulyssetsd@users.noreply.github.com>
2023-04-14 21:58:33 +0200

    minor renaming (#36)

--- RevoProfit.Core/Crypto/Services/Interfaces/ICryptoService.cs
+++ RevoProfit.Core/Crypto/Services/Interfaces/ICryptoService.cs
@@ -5,5 +5,5 @@ namespace RevoProfit.Core.Crypto.Services.Interfaces;
 public interface ICryptoService
 {
     (IReadOnlyCollection<CryptoAsset>, IReadOnlyCollection<CryptoSell>, IReadOnlyCollection<CryptoFiatFee>) ProcessTransactions(IEnumerable<CryptoTransaction> transactions);
-    IEnumerable<CryptoReport> MapToReports(IEnumerable<CryptoSell> cryptoRetraits, IEnumerable<CryptoFiatFee> cryptoFiatFees);
+    IEnumerable<CryptoReport> MapToReports(IEnumerable<CryptoSell> cryptoSells, IEnumerable<CryptoFiatFee> cryptoFiatFees);
 }
